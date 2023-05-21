fede4bf39974e87b416c99baef59dec36e22ebcb
Alex Figueroa <alexfigueroa.cybr@gmail.com>
2023-05-21 14:58:20 -0700

    refactors the following as classes, and uses roles to name files: DirectoryManager, CodebaseScanner

--- codebase_craft/dynamic_codebase_templating/codebase_scanning.py
+++ codebase_craft/dynamic_codebase_templating/codebase_scanning.py
@@ -1,19 +1,74 @@
-# dynamic_codebase_templating/codebase_scanning.py
-
 import os
+from codebase_craft.utils.handlers import (
+    log_info,
+    log_error,
+    print_success,
+    print_info,
+    start_progress_task,
+    update_progress,
+)
 
 
-def scan_directory():
-    """
-    Scan a directory and generate an outline of the codebase.
-    If no directory is specified, scan the current directory.
-    """
-    outline = {}
-    exclude_dirs = {".venv", "node_modules", ".vscode", "*.egg-info", ".git"}
+class CodebaseScanner:
+    def __init__(self):
+        self.outline = {}
+        self.languages = set()
 
-    for root, dirs, files in os.walk("."):
-        dirs[:] = [d for d in dirs if d not in exclude_dirs]
-        for file in files:
-            # Add file to outline
-            outline[file] = os.path.join(root, file)
-    return outline
+    def scan(self, root="."):
+        exclude_dirs = {
+            ".venv",
+            "node_modules",
+            ".vscode",
+            "*.egg-info",
+            ".git",
+            "*.cache",
+        }
+
+        log_info("Scanning directory...")
+
+        total_files = sum([len(files) for r, d, files in os.walk(root)])
+        progress_task = start_progress_task(total_files, "Scanning files")
+
+        for root, dirs, files in os.walk(root):
+            dirs[:] = [d for d in dirs if d not in exclude_dirs]
+            for file in files:
+                self.outline[file] = os.path.join(root, file)
+                update_progress(progress_task)
+
+                if file.endswith(".py"):
+                    self.languages.add("Python")
+                elif file.endswith(".js"):
+                    self.languages.add("JavaScript")
+
+        self.outline["languages"] = list(self.languages)
+
+        print_success("Scanning completed!")
+        return self.outline
+
+    def get_languages(self):
+        return list(self.languages)
+
+    def get_directory_outline(self):
+        return self.outline
+
+    def print_to_console(self):
+        print_info("Directory Outline:")
+        for file, path in self.outline.items():
+            print_info(f"{file}: {path}")
+
+        print_info("\nLanguages Detected:")
+        for language in self.languages:
+            print_info(language)
+
+    def gather_metadata(self):
+        metadata = {
+            "num_files": len(self.outline),
+            "num_python_files": len(
+                [file for file in self.outline if file.endswith(".py")]
+            ),
+            "num_js_files": len(
+                [file for file in self.outline if file.endswith(".js")]
+            ),
+            "languages": list(self.languages),
+        }
+        return metadata
